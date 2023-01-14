779d17a6eb131598125376a6ed88fd4472b399f6
unknown <ed_fern@hotmail.com>
2023-01-14 13:10:44 -0600

    starting project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # push_notifications
+
 Let's explore how we can send push notifications using Spring Boot and Websockets.
---
+++ src/test/java/com/defv/pushnotifications/PushNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defv.pushnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
